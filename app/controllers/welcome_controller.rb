b7aa1a186e4bde6e57b6c75e7e26be73dc707c44
César Díaz <cesar.diaz@toptal.com>
2018-06-07 20:00:47 -0300

    adding test and gmail scrapper module

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,20 @@ require 'google/apis/gmail_v1'
 
 class WelcomeController < ApplicationController
   def index
-    gmail = Gmail.connect(:xoauth2, current_user.email, current_user.oauth_token)
-    @search = 'paypal Recibo de su pago realizado'
-    @emails = gmail.mailbox('Mis Mails').emails(gm: @search)
+    begin
+      gmail = Gmail.connect(:xoauth2, current_user.email, current_user.oauth_token)
+      @search = 'paypal Recibo de su pago realizado'
+      emails = gmail.inbox.emails(gm: @search)
 
+      @paypal_transactions = []
+      emails.each do |email|
+        @paypal_transactions <<  GmailScrapper.paypal(email.body)
+      end
+      @paypal_transactions.reverse!
+
+    rescue Exception => e
+      @error = 'An error with gmail connection happened. Re login again please'
+      @paypal_transactions = []
+    end
   end
 end
