6854faab36ff636b9b1035023220dc992bd25e0a
César Díaz <cesar.diaz@toptal.com>
2018-06-09 12:26:35 -0300

    added gmail service

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,13 @@
-require 'google/apis/gmail_v1'
-
 class WelcomeController < ApplicationController
-  Gmail = Google::Apis::GmailV1
-
   def index
     @transactions = Transaction.all
   end
 
   def refresh_transactions
-    Transaction.search_for_transactions(current_user)
-    redirect_to root_path
+    if TransactionsService.new(current_user).call()
+      redirect_to root_path, notice: 'Refresh done!'
+    else
+      redirect_to destroy_user_session_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_08_124318) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "transactions", force: :cascade do |t|
     t.integer "user_id"
     t.string "company"
@@ -45,7 +48,6 @@ ActiveRecord::Schema.define(version: 2018_06_08_124318) do
     t.datetime "oauth_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
